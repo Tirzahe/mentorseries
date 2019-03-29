25464d9ff7199d5b680fa423f0a227d690f52713
Tirzah Evangelista <tirzah410@gmail.com>
2019-03-29 00:27:01 -0600

    adjust funtions to use forEach.

--- src/familyStats.js
+++ src/familyStats.js
@@ -11,21 +11,26 @@
 */
 function oldest(family){
     let oldestPerson = { 
-        name: "", 
+        name: "I'm younger than you think", 
         age: 0 
         };
-    for (i = 0; i < family.length; i++){
-        if (family[i].age > oldestPerson.age){
-            oldestPerson = family[i];
+    // for (i = 0; i < family.length; i++){
+    //     if (family[i].age > oldestPerson.age){
+    //         oldestPerson = family[i];
+    //     }
+    // } this works but I want to try writing a forEach so it's commented out for testing.
+    family.forEach(function(person){
+        if(person.age > oldestPerson.age){
+            oldestPerson = person;
         }
-    }
+    })
     return oldestPerson; //The name of the oldest member of that family
 }
 
 function youngest(family){
     let youngestPerson = {
-        name: "I'm Young",
-        age: 1000
+        name: "I'm not that Young",
+        age: Infinity
     };
     family.forEach(function(person){
         if(person.age < youngestPerson.age){
@@ -34,22 +39,17 @@ function youngest(family){
     })
     return youngestPerson;
 }
-
-const oldestName = oldest([
-    { name: 'Matt', age: 36 },
-    { name: 'Tara', age: 34 },
-    { name: 'Analice', age: 52 },
-    { name: 'Rachel', age: 9 },
-]); // -> 'Matt'
-console.log("Hello " + oldestName.name + ", you're the oldest!");
-
-const youngestName = youngest([
+ let myTestFamily = [
     { name: 'Matt', age: 36 },
     { name: 'Sarah', age: 3 },
     { name: 'Tara', age: 34 },
     { name: 'Analice', age: 52 },
     { name: 'Rachel', age: 9 },
-]); // -> 'Rachel'
+];
+const oldestName = oldest(myTestFamily); // -> 'Analice'
+console.log("Hello " + oldestName.name + ", you're the oldest!");
+
+const youngestName = youngest(myTestFamily); // -> 'Sarah'
 console.log("Hello " + youngestName.name + ", you're the youngest!");
 
 module.exports = { oldest, youngest };
